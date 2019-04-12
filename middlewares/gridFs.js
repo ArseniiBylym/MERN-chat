21d1514a953ca39e74ed3d616130e1a6b36a181c
Arsenii Bylym <arseniibylym@gmail.com>
2019-04-12 16:48:44 +0300

    Update file name in GridFs storage

--- middlewares/gridFs.js
+++ middlewares/gridFs.js
@@ -1,16 +1,25 @@
 const path = require('path');
+const crypto = require('crypto')
 const multer = require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
 
 const storage = new GridFsStorage({
     url: process.env.MONGO_DB_URI,
     file: (req, file) => {
-        const fileName = req.user.id + path.extname(file.originalname)
-        return {
-                filename: fileName,
-                bucketName: 'avatars'
-        };
-    }
+        return new Promise((resolve, reject) => {
+          crypto.randomBytes(16, (err, buf) => {
+            if (err) {
+              return reject(err);
+            }
+            const filename = buf.toString('hex') + path.extname(file.originalname);
+            const fileInfo = {
+              filename: filename,
+              bucketName: 'avatars'
+            };
+            resolve(fileInfo);
+          });
+        });
+      }
 });
 
   module.exports = multer({ storage });
