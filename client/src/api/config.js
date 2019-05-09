59e02ac9bc82834ec9dd92eb13d5dbbc873300ff
Arsenii Bylym <arseniibylym@gmail.com>
2019-05-09 20:16:19 +0300

    Fix base_usi for api

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,1 +1,6 @@
-export const BASE_URI = `http://localhost:5000`;
+export const BASE_URI = (() => {
+    if (process.env.NODE_ENV === 'development') {
+        return 'http://localhost:5000';
+    }
+    return '';
+})();
